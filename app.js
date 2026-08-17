09e1c589ad8bda2824e50777d8a74f92dfc2bc55
Shruti Narkhede <spnarkhede2003@gmail.com>
2026-08-17 19:17:40 +0530

    Bump version to v1.0.2 to test automatic SCM polling

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 // Home route
 app.get("/", (req, res) => {
-    res.send("🚀 Jenkins CI/CD Pipeline is working! Webhook test successful! (v1.0.1)");
+    res.send("🚀 Jenkins CI/CD Pipeline is working! Webhook test successful! (v1.0.2)");
 });
 
 // Health check route
